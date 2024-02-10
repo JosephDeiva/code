9fbc7d1b1e7c16571bbf08236c1f43b255b5bcd0
joseph <josephdeiva15082@gmail.com>
2024-02-10 19:28:44 +0530

    uncommented

--- 1.c/Array.c
+++ 1.c/Array.c
@@ -7,10 +7,10 @@
 //            |   ---------
 //size of element  array element */
  
-// #include<stdio.h>
-// int main()
-// {
-//     int a[5]={1,2,3,4,5};
-//     printf("%d",a[3]);
-// }
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    printf("%d",a[3]);
+}
 
